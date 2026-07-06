2e6724f72ee3190fb6dff36862636ac7366128dc
adam5192 <adammokdad2003@gmail.com>
2026-07-06 13:34:49 -0400

    Update MapView.js

--- src/views/MapView.js
+++ src/views/MapView.js
@@ -8,15 +8,29 @@ export class MapView {
     this._map = null;
   }
 
-  render(coords = [51.505, -0.09]) {
+  render(activities = [], countryCoords = null) {
     // remove existing map first - re-render
     if (this._map) {
       this._map.remove();
       this._map = null;
     }
-    this._map = L.map("map").setView(coords, 6);
+    this._map = L.map("map");
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "© OpenStreetMap contributors",
     }).addTo(this._map);
+
+    // drop pins for activities that have coords
+    const withCoords = activities.filter((a) => a.coords);
+
+    withCoords.forEach((a) => {
+      L.marker(a.coords).addTo(this._map).bindPopup(`${a.name} (${a.type})`);
+    });
+
+    // decide what area to show
+    if (withCoords.length > 0) {
+      // fit map to show all pins
+      const bounds = withCoords.map((a) => a.coords);
+      this._map.fitBounds(bounds, { padding: [50, 50] });
+    }
   }
 }
