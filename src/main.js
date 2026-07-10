f2d21caa98d8cad71592c2a299536cd2bbd01f40
adam5192 <adammokdad2003@gmail.com>
2026-07-10 17:45:48 -0400

    remove unused imports

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-"use strict";
-import { App } from "./app.js";
-import { Trip } from "./model/Trip.js";
-import { save, load } from "./data/store.js";
-import { Activity } from "./model/Activity.js";
 import "leaflet/dist/leaflet.css";
-import { searchPlaces } from "./data/geocode.js";
+import { App } from "./app.js";
 
 const app = new App();
